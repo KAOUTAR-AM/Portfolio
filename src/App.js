0aeae83c4327149eedba80295e090093bcbae304
KAOUTAR AMAZZAR <kaoutaramazzar@gmail.com>
2024-03-25 16:29:31 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';   
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Skills from './components/Skills/Skills';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <Navbar/>
+      <div className='container'>
+        <Hero/>
+        <Skills/>
+      </div>
+      
+      {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +27,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>  */}
     </div>
   );
 }
